ac5c8eefd412817b7f4cb9a8b2644e23d4762bf0
abertot <bertotalexandre82@gmail.com>
2024-12-01 09:42:03 +0100

    Deuxième partie de la première journée du AOC

--- day 1/code_day_1.py
+++ day 1/code_day_1.py
@@ -16,5 +16,22 @@ def calcul_distance(fichier="puzzleday1.txt") :
         return 0
     
 
+def calcul_similarity(fichier="puzzleday1.txt") :
+    try: 
+        with open(fichier, 'r') as lignes :
+            score = 0
+            data = [int(location) for ligne in lignes for location in ligne.strip().split()]
+            location1 = data[0::2]
+            location2 = data[1::2]
+            for location in location1 : 
+                score += location * location2.count(location)
+            return score
+    except FileNotFoundError:
+        print(f"Le fichier {fichier} est introuvable")
+        return 0
+    except ValueError:
+        print("Le fichier contient des données non valides.")
+        return 0
+    
 print(calcul_distance("puzzleday1.txt"))
-
+print(calcul_similarity("puzzleday1.txt"))
